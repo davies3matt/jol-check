188a39af618ee4d4a1bb117ae7b8ee2cc43aa2df
Matt Davies <matthew.davies@sovtech.com>
2022-01-09 20:32:11 +0200

    feat(apollo): add apollo client config and provider

--- App.tsx
+++ App.tsx
@@ -1,36 +1,31 @@
+import { ApolloProvider } from '@apollo/client';
 import React from 'react';
 import Amplify from 'aws-amplify';
 import { NativeBaseProvider } from 'native-base';
 import awsconfig from './src/aws-exports';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-/** Graphql */
-import { createUser } from './src/graphql/mutations'
-import { listUsers } from './src/graphql/queries';
-import Home from './src/containers/Home';
-import Login from './src/containers/Authentication/Login';
-import SignUp from './src/containers/Authentication/SignUp';
-
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import client from './src/apollo';
+import AuthProvider from './src/context/AuthContext';
+import AppNavigation from './src/navigation';
 
 Amplify.configure(awsconfig);
 
-const Stack = createNativeStackNavigator();
+const config = {
+  dependencies: {
+    'linear-gradient': require('expo-linear-gradient').LinearGradient
+  }
+}
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <NativeBaseProvider>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SignUp" component={SignUp} />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ title: 'Home' }}
-        />
-      </Stack.Navigator>
-      </NativeBaseProvider>
-    </NavigationContainer>
+    // @ts-ignore
+    <ApolloProvider client={client}>
+        <NativeBaseProvider config={config}>
+        <AuthProvider>
+          <AppNavigation/>
+        </AuthProvider>
+        </NativeBaseProvider>
+    </ApolloProvider>
   );
 }
 
