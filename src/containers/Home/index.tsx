d7fe2a37686ccbce835db11227aa6ac95f49dafe
Matt Davies <matthew.davies@sovtech.com>
2022-01-09 20:35:53 +0200

    feat((home): add routing options and splash animation

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,15 +1,43 @@
 import { Button } from 'native-base';
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { useAuthContext } from '../../context/AuthContext';
 
 interface Props {
     navigation: any;
 }
 const Home: React.FC<Props> = ({navigation}) => {
+    const { signOut } = useAuthContext();
+    const [animation , setAnimation] = useState<any>();
+
+    React.useEffect(() => {
+        if (animation) {
+            animation.play();
+        }
+     },[animation]);
+
     return(
-        <View>
-            <Text>Home Page</Text>
-            <Button onPress={() => navigation.navigate('Login')}>Press Me</Button>
+        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <LottieView
+                ref={animation => {
+                    setAnimation(animation);
+                }}
+                style={{
+                    width: 400,
+                    height: 400,
+                }}
+                loop={false}
+                source={require('../../../assets/animations/home.json')}
+            
+            />
+            <Button style={{width: 100, marginTop: 25}} onPress={() => navigation.navigate('Jols')}>Jols</Button>
+            <Button style={{width: 100, marginTop: 10}} onPress={() => navigation.navigate('Profile')}>Profile</Button>
+            <Button
+                style={{width: 100, marginTop: 10}}
+             onPress={() => {
+                signOut();
+            }}>Sign Out</Button>
         </View>
     )
 }
